45be2649a953baa4d4810c7c5c69a6b2ea77200c
Harshit Ahuja <ahujaharshit26@gmail.com>
2024-07-24 13:48:45 +0530

    async handler error and response utilities added

--- scr/index.js
+++ scr/index.js
@@ -1,7 +1,16 @@
 
+import { app } from "./app.js";
 import connectDb from "./db/index.js";
 
 
 
 // connect db
 connectDb()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running`)
+    })
+  })
+  .catch((err) => {
+    console.log(`db connecttion failed ${err}`)
+  })
